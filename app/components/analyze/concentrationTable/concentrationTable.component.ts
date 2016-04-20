4542181b0ea523aaecc9d0866085fbdcaa55a299
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-04-20 12:22:58 +0300

    concentrationTable updated

--- app/components/analyze/concentrationTable/concentrationTable.component.ts
+++ app/components/analyze/concentrationTable/concentrationTable.component.ts
@@ -4,7 +4,12 @@ import {MetaboliteConcentration} from '../../../services/analyze.service';
 @Component({
     selector: 'concentration-table',
     templateUrl: '/app/components/analyze/concentrationTable/concentrationTable.html',
+    styleUrls: ['app/components/analyze/concentrationTable/concentrationTable.css']
 })
 export class ConcentrationTableComponent {
     @Input() conTable: Array<MetaboliteConcentration>;
+
+    remove(index) {
+      this.conTable.splice(index,1);
+    }
 }
