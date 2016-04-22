86ca11ac2f79049f0ced15f75c2d1060fe787d4f
muhammed hasan celik <hasancelik@std.sehir.edu.tr>
2016-04-22 17:27:15 +0300

    analyze

--- app/components/analyze/concentrationTable/concentrationTable.component.ts
+++ app/components/analyze/concentrationTable/concentrationTable.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from 'angular2/core';
 import {MetaboliteConcentration} from '../../../services/analyze.service';
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/common';
 
 @Component({
     selector: 'concentration-table',
@@ -9,7 +10,34 @@ import {MetaboliteConcentration} from '../../../services/analyze.service';
 export class ConcentrationTableComponent {
     @Input() conTable: Array<MetaboliteConcentration>;
 
-    remove(index) {
-      this.conTable.splice(index,1);
+    concenration = ['Increase Slightly', 'Increase Dyramaticly',
+        'Decrease Slightly', 'Decrease Dyramaticly', 'Exact Value'];
+
+    form: ControlGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.form = this.createForm();
     }
+
+    remove(index) {
+        this.conTable.splice(index, 1);
+    }
+
+    createForm() {
+        return this.fb.group({
+            "name": ["", Validators.required],
+            "concentration": ["", Validators.required],
+            "value": [""]
+        });
+    }
+
+    onSubmit(value) {
+        let c = new MetaboliteConcentration();
+        c.name = value['name'];
+        c.concentration = value['concentration'];
+        c.exactValue = value['value'];
+        this.conTable.push(c);
+        this.form = this.createForm();
+    }
+
 }
