98a65d1805bb4e306049df52950558f1ef05a20d
abdullahsecer <abdullahsecer@std.sehir.edu.tr>
2016-04-23 14:34:33 +0300

    relocated providers to boot.ts

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -3,15 +3,11 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {AnalyzeComponent} from './analyze/analyze.component';
 import {SearchComponent} from './search/search.component';
 import {DetailsComponent} from './details/details.component';
-import {MetaboliteService} from '../services/metabolite.service';
-import {ReactionService} from '../services/reaction.service';
-
 
 @Component({
     selector: 'app',
     templateUrl: '/app/components/app.html',
     directives: [ROUTER_DIRECTIVES],
-    providers: [MetaboliteService, ReactionService]
 })
 @RouteConfig([
     {
