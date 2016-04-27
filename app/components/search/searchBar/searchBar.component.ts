8cd4ac254fe5eb66c58f66ac42a57debf331ddf0
ahmetesat <ahmetkara@std.sehir.edu.tr>
2016-04-27 03:42:56 +0300

    Added files via upload
    Search bar generated

--- app/components/search/searchBar/searchBar.component.ts
+++ app/components/search/searchBar/searchBar.component.ts
@@ -1,13 +1,58 @@
-import {Component} from 'angular2/core';
+import {Component,ElementRef} from 'angular2/core';
 
 @Component({
     selector: 'search-bar',
+    host: {
+        '(document:click)': 'handleClick($event)',
+    },
+
     templateUrl: '/app/components/search/searchBar/searchBar.html',
 })
 export class SearchBarComponent {
     searchTerm: String;
+    query = '';
+    countries = [ "Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus",
+                       "Belgium","Bosnia & Herzegovina","Bulgaria","Croatia","Cyprus",
+                       "Czech Republic","Denmark","Estonia","Finland","France","Georgia",
+                       "Germany","Greece","Hungary","Iceland","Ireland","Italy","Kosovo",
+                       "Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta",
+                       "Moldova","Monaco","Montenegro","Netherlands","Norway","Poland",
+                       "Portugal","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia",
+                       "Spain","Sweden","Switzerland","Turkey","Ukraine","United Kingdom","Vatican City"];
+    public elementRef;
+    public filteredList = [];
+    constructor(myElement: ElementRef) {
+        this.elementRef = myElement;}
 
-    constructor() { }
 
     search() { }
+    filter() {
+    if (this.query !== ""){
+        this.filteredList = this.countries.filter(function(el){
+            return el.toLowerCase().indexOf(this.query.toLowerCase()) === 0;
+        }.bind(this));
+    }else{
+        this.filteredList = [];
+    }
+}
+
+select(item){
+    this.query = item;
+    this.filteredList = [];
+}
+
+handleClick(event){
+   var clickedComponent = event.target;
+   var inside = false;
+   do {
+       if (clickedComponent === this.elementRef.nativeElement) {
+           inside = true;
+       }
+      clickedComponent = clickedComponent.parentNode;
+   } while (clickedComponent);
+    if(!inside){
+        this.filteredList = [];
+    }
+}
+
 }
