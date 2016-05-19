5fd7e7670af9d01bc981480b3395358040ace312
Muhammed Hasan <hasancelik@std.sehir.edu.tr>
2016-05-19 15:16:27 +0300

    Unnecessary code of my friend cleaned

--- app/pipes/keys.pipe.ts
+++ app/pipes/keys.pipe.ts
@@ -1,9 +1,6 @@
 import {Pipe, PipeTransform} from 'angular2/core';
 
-
-@Pipe({ name: 'Keys'})
+@Pipe({ name: 'keys' })
 export class KeysPipe implements PipeTransform {
-    transform(value: { [key: string]: string[] | string }): Object {
-        return Object.keys(value).map(key => key);
-    }
+    transform = (value: any) => Object.keys(value);
 }
